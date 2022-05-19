bbbd573b0cc56911f7b82378c165a289993ffbbe
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2022-05-19 13:33:22 +0200

    start reading buffers

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use hound;
 use std::env;
 use std::io;
 
+const BUFSIZE : usize = 32;
+
 /// Compute the RMS of either integers or float samples.
 fn compute_rms<S, R>(reader: &mut hound::WavReader<R>) -> f64
 where
@@ -10,21 +12,29 @@ where
     S: hound::Sample,
     R: io::Read,
 {
-    let sqr_sum = reader.samples::<S>().fold(0.0, |sqr_sum, s| {
-        let sample = f64::from(s.unwrap());
-        sqr_sum + sample * sample
-    });
-    (sqr_sum / reader.len() as f64).sqrt()
+
+    let mut buf = Vec::<f64>::with_capacity(BUFSIZE);
+    for sample in reader.samples::<S>() {
+        if sample.is_ok() {
+            buf.push(f64::from(sample.unwrap()));
+            if buf.len() == BUFSIZE {
+                handle_buffer(&buf);
+                buf.clear();
+            }
+        }
+    }
+    0.0
+}
+
+fn handle_buffer(buf: &[f64]) {
+    println!("{:?}", buf);
 }
 
 fn main() {
-    // Compute the RMS for all files given on the command line.
-    for fname in env::args().skip(1) {
-        let mut reader = hound::WavReader::open(&fname).unwrap();
-        let rms = match reader.spec().sample_format {
-            hound::SampleFormat::Float => compute_rms::<f32, _>(&mut reader),
-            hound::SampleFormat::Int => compute_rms::<i32, _>(&mut reader),
-        };
-        println!("{}: {:0.1} ({} samples)", fname, rms, reader.len());
-    }
+    let mut reader = hound::WavReader::open("C.wav").unwrap();
+    let pitch = match reader.spec().sample_format {
+        hound::SampleFormat::Float => compute_rms::<f32, _>(&mut reader),
+        hound::SampleFormat::Int => compute_rms::<i32, _>(&mut reader),
+    };
+    println!("pitch: {}", pitch);
 }
